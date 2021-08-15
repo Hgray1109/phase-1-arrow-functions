596882437307fb597260ed2c25e3b46e6f4ce3ee
Hashem Gray <Hgray1109@gmail.com>
2021-08-15 09:09:56 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function divide() {
+    return 2000/100;
+}
+
+const square = squaretest => (x * x)
+    let x =2
+    square()
+
+const add = (a, b) => a + b
+let a =3
+let b =4
+add(a,b)
